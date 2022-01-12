e2f418abf6cb05064c4dd09afca440a1035b1880
ManukumarNM <manunm.jps@gmail.com>
2022-01-12 17:25:06 +0530

    make it better

--- Laptop_API/urls.py
+++ Laptop_API/urls.py
@@ -13,10 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from items_app.views import UserViewSet, LaptopsView
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('items_app.urls')),
+    #path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
+
+    path('item/', include('items_app.urls')),
 ]
